170400a391fc8b402c3c8b653ade15c9f886a96a
Carlos Barreto <129400612+dev-cerb@users.noreply.github.com>
2026-05-06 20:25:37 +0000

    `feat:` add type and data task in mvp

---
+++ frontend/src/data/mockTasks.ts
@@ -0,0 +1,11 @@
+import type Task from "../types/task";
+
+const mockTasks: Task[] = [
+  { id: 1, title: "Task 1", description: "", status: "todo" },
+  { id: 2, title: "Task 2", description: "", status: "doing" },
+  { id: 3, title: "Task 3", description: "", status: "done" },
+  { id: 4, title: "Task 4", description: "", status: "todo" },
+  { id: 5, title: "Task 5", description: "", status: "doing" },
+];
+
+export default mockTasks;
